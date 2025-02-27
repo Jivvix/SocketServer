5090f659e2d8cbb7012db41dd95d4da1ebe61641
aaa28 <aaa28@DRAGON-ARMY>
2025-02-27 13:45:53 +0700

    Initial commit

---
+++ src/SocketServer.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+    public class SocketServer {
+        public static void main(String[] args) throws IOException {
+            ServerSocket serverSocket = new ServerSocket(9000);
+            System.out.println("Открыт сокет на порту 9000, ждём подключения");
+            Socket clientSocket = serverSocket.accept(); // открывает сокет и блокируется пока не будет подключения
+            System.out.println("Подключен клиент");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println("Получена строка: " + inputLine);
+            }
+        }
+    }
